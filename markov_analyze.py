de12a8b62e7bd05ea00349876f0c347a9643f9f6
swerwath <swerwath@berkeley.edu>
2016-04-11 22:36:06 -0700

    Begin switching to Ruby

--- markov_analyze.py
+++ markov_analyze.py
@@ -8,7 +8,9 @@ probability_dict = {}
 
 txt = open(args.text_file)
 for line in txt:
-        for word in line.split():
-           probability_dict[word] = {}
+    for word in line.split():
+        if not word in probability_dict:
+            probability_dict[word] = {}
+        
 
 print("Found " + str(len(probability_dict)) + " unique words in " + args.text_file)
