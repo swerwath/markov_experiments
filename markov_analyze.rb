23d0b07434d19340d48edbf5e025ffe793f22a06
swerwath <swerwath@berkeley.edu>
2016-04-11 23:52:48 -0700

    Added console output to data calculation

--- markov_analyze.rb
+++ markov_analyze.rb
@@ -7,7 +7,9 @@ words = File.read(text_file).scan(/\w+/)
 words = words.map{ |w| w.downcase }
 
 transition_hash = transition_counts words
+puts "Finished counting, found #{transition_hash.count} unique words..."
 transition_probabilities transition_hash
+puts "Finished calculatating probability hash"
 
 if text_file.rindex('/').nil?
   target_file_path = text_file[0...text_file.rindex('.')]
@@ -16,7 +18,10 @@ else
 end
 target_file_path = "analyzed_data/"+target_file_path+".yaml"
 
+puts "Writing data to #{target_file_path}"
 serialized_object = YAML::dump(transition_hash)
 File.open(target_file_path, "w") do |file|
   file.puts serialized_object
 end
+
+puts "Done!"
